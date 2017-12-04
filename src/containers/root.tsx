970d50cfee617180954c78cf882ce0868a090d08
Tom Wright <tom.w.wright@live.com>
2017-12-04 19:10:16 +1100

    Rebuild UI with Material UI

--- src/containers/root.tsx
+++ src/containers/root.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 
+import { MuiThemeProvider } from 'material-ui/styles';
+
+import theme from 'theme';
+
+import App from 'containers/app';
 import Memory from 'components/memory';
 import AssemblyEditor from 'containers/assemblyeditor';
 
 const Root: React.StatelessComponent<{}> = () => (
-  <div>
-    <Memory />
-    <AssemblyEditor />
-  </div>
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>
 );
 
 export default Root;
