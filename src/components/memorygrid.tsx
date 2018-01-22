550500518414194020c9199f1ab2a3d504eb8369
Tom Wright <tom.w.wright@live.com>
2018-01-22 22:34:16 +1100

    Add styling to active memory cell for when compute not running

--- src/components/memorygrid.tsx
+++ src/components/memorygrid.tsx
@@ -20,13 +20,25 @@ const styles = withStyles<string>(theme => ({
     margin: '4px',
     width: '50px',
   },
+  programCountInactiveMemoryCell: {
+    border: `1px solid ${theme.palette.grey.A400}`,
+    margin: '4px',
+    width: '50px',
+  },
 }));
 
 const MemoryGrid: React.StatelessComponent<WithStyles & MemoryProps> = ({ computeStore, classes }) => (
   <Scrollable height={200}>
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       {computeStore.memory.map((memory, i) => (
-        <Paper key={i} className={computeStore.counter == i ? classes.programCountMemoryCell : classes.memoryCell}>
+        <Paper
+          key={i}
+          className={
+            computeStore.counter == i
+              ? computeStore.isRunning ? classes.programCountMemoryCell : classes.programCountInactiveMemoryCell
+              : classes.memoryCell
+          }
+        >
           <ListItemText primary={('00' + memory).slice(-3)} secondary={('0' + i).slice(-2)} />
         </Paper>
       ))}
